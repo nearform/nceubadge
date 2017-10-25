242137f228b81f619471d1f50db13c82754810f7
Gordon Williams <gw@pur3.co.uk>
2017-10-25 12:38:57 +0100

    add flash script

---
+++ hardware/flash_firmware.sh
@@ -0,0 +1,4 @@
+/bin/bash
+
+# http://infocenter.nordicsemi.com/index.jsp?topic=%2Fcom.nordic.infocenter.tools%2Fdita%2Ftools%2Fnrf5x_command_line_tools%2Fnrf5x_command_line_tools_lpage.html
+nrfjprog --family NRF52 --clockspeed 50000 --program firmware.hex --chiperase --reset
